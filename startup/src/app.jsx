3e0079dbb3911b2a1ef90ed87208559baf5a717b
santi <santi2811b@gmail.com>
2024-11-07 17:52:24 -0700

    added components, barely starting

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { About } from './about/about';
+import { Habits } from './habits/habits';
 
 export default function App() {
     return (
+        <BrowserRouter>
       <div className='body bg-dark text-light'>
         <header className='container-fluid'>
-          <nav className='navbar fixed-top navbar-dark'>
+                    <nav className='navbar fixed-top navbar-dark'>
             <div className='navbar-brand'>
-              HabitApp<sup>&reg;</sup>
+                HabitApp<sup>&reg;</sup>
             </div>
             <menu className='navbar-nav'>
-              <li className='nav-item'>
-                <a className='nav-link' href='index.html'>
-                  Home
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='habits.html'>
-                  Habits
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='about.html'>
-                  About
-                </a>
-              </li>
+                <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                    Login
+                </NavLink>
+                </li>
+                <li className='nav-item'>
+                <NavLink className='nav-link' to='habits'>
+                    Habits
+                </NavLink>
+                </li>
+                <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                    About
+                </NavLink>
+                </li>
             </menu>
-          </nav>
+            </nav>
         </header>
   
-        <main>App components go here</main>
-        
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/habits' element={<Habits />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+
         <footer className='bg-dark text-white-50'>
           <div className='container-fluid'>
             <span className='text-reset'>Santiago Bolano</span>
@@ -41,5 +51,9 @@ export default function App() {
           </div>
         </footer>
       </div>
+      </BrowserRouter>
     );
+  }
+  function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
   }
---
+++ startup/src/habits/habits.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Habits() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>habits displayed here</div>
+    </main>
+  );
+}
