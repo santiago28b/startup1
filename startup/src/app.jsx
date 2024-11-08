70667799121d51d6cae897b870c2e8421e76a36e
santi <santi2811b@gmail.com>
2024-11-07 17:25:20 -0700

    Update css and adde more stuff

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -3,5 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              HabitApp<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link' href='index.html'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='habits.html'>
+                  Habits
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='about.html'>
+                  About
+                </a>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <main>App components go here</main>
+        
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Santiago Bolano</span>
+            <a className='text-reset' href='https://github.com/santiago28b/startup1/tree/main'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
