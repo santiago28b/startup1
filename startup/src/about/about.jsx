83bf71db616410460ffdf568710a2586dd8af241
santi <santi2811b@gmail.com>
2024-11-19 14:56:59 -0700

    service done

--- startup/src/about/about.jsx
+++ startup/src/about/about.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
 
 export function About() {
+
+  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+
+  // We only want this to render the first time the component is created and so we provide an empty dependency list.
+  React.useEffect(() => {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#picture');
+
+        const defaultWidth = 600;
+        const defaultHeight = 400;
+
+        const width = containerEl?.offsetWidth || defaultWidth;
+        const height = containerEl?.offsetHeight || defaultHeight;
+
+        const apiUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        setImageUrl(apiUrl);
+      })
+      .catch();
+
+    fetch('https://quote.cs260.click')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setQuoteAuthor(data.author);
+      })
+      .catch();
+  }, []);
+
   return (
     <main className='container-fluid bg-secondary text-center'>
         <p className="lead" style={{color:"black"}} > 
@@ -9,14 +42,16 @@ export function About() {
         <p className="h1" style={{color:"black"}}>Motivational quote of the day</p>
         
         <hr></hr>
-        <img src="https://www.campervannewzealand.co.nz/assets/img/blog/444/Mount_Taranaki.jpg" className="img-fluid" alt="landscape"></img>
+        <div id='picture' style={{ width: '600px', height: '400px' }}>
+        <img  src={imageUrl} className="img-fluid" style={{width:'100%', height:'100%'}} alt="stock background"></img>
+        </div>
         <br></br>
         <figure className="text-center">
             <blockquote className="blockquote">
-              <p style={{color:"black"}}>"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful."</p>
+              <p style={{color:"black"}}>{quote}</p>
             </blockquote>
             <figcaption className="blockquote-footer">
-                Albert Schweitzer
+               {quoteAuthor}
             </figcaption>
           </figure>
     </main>
