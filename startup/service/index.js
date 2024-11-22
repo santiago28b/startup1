a89ced7656f36e63c58cd87534eff5c40a16fd5a
santi <santi2811b@gmail.com>
2024-11-21 17:30:27 -0700

    cookies created

--- startup/service/index.js
+++ startup/service/index.js
@@ -13,6 +13,7 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
+app.set('trust proxy', true);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -25,7 +26,7 @@ let habits = [
   { id: uuid.v4(),name: 'save $1000'},
 ];
 
-var apiRouter = express.Router();
+const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
 // CreateAuth a new user without monggo
@@ -54,28 +55,62 @@ apiRouter.post('/auth/create',async (req,res)=> {
   
 })
 
-  // GetAuth login an existing user
-apiRouter.post('/auth/login', async (req, res) => {
-    const user = users[req.body.userName];
-    if (user) {
-      if (req.body.password === user.password) {
-        user.token = uuid.v4();
-        res.send({ token: user.token });
-        return;
-      }
-    }
-    res.status(401).send({ msg: 'Unauthorized' });
-  });
+  // GetAuth login an existing user without mongo
+// apiRouter.post('/auth/login', async (req, res) => {
+//     const user = users[req.body.userName];
+//     if (user) {
+//       if (req.body.password === user.password) {
+//         user.token = uuid.v4();
+//         res.send({ token: user.token });
+//         return;
+//       }
+//     }
+//     res.status(401).send({ msg: 'Unauthorized' });
+//   });
 
-  // DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-      delete user.token;
+apiRouter.post('/auth/login', async (req,res)=>{
+  const user = await DB.getUser(req.body.user);
+  if(user){
+    if(req.body.password === user.password){
+      sethAuthCookie(res,user.token);
+      res.send({id: user._id})
+      return;
     }
-    res.status(204).end();
-  });
+  }
+  res.status(401).send({msg: 'Unauthorized'});
+})
+
+
+  // DeleteAuth logout a user without mongo
+// apiRouter.delete('/auth/logout', (req, res) => {
+//     const user = Object.values(users).find((u) => u.token === req.body.token);
+//     if (user) {
+//       delete user.token;
+//     }
+//     res.status(204).end();
+//   });
 //down you will find mongoDB
+
+//with mongo
+apiRouter.delete('auth/logout', (req,res)=>{
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+})
+
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+
 
 // GetHabits
 apiRouter.get('/habits', async (_req, res) => {
